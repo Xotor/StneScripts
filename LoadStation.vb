5b7dde3d9ee343adbf21611ca20165daddb62f02
Xotor <xotor@gmx.de>
2017-02-21 09:04:17 +0100

    Created LoadStation.vb
    This Script loads the WarpCore with Deuterium from the Colony below.

---
+++ LoadStation.vb
@@ -0,0 +1,20 @@
+Var MinDeutOnColony As Integer = 1000;
+
+Var SpaceStationID As Integer = 1940125;
+Var ColonyID As Integer = 35662;
+
+Var Station As New CMyShip(SpaceStationID);
+Var Colony As New CMyColony(ColonyID);
+
+WriteLine(Colony.Name & " has " & Colony.StockRoom.Amount(EGoodsType.Deuterium) & " Deuterium");
+WriteLine(Station.Name & " has " & Station.StockRoom.Amount(EGoodsType.Deuterium) & " Deuterium");
+
+If(Station.StockRoom.Amount(EGoodsType.Deuterium) > 0 AND (Station.Definition.WarpCore - Station.WarpCore) > 1){
+  Station.Action.RefillWarpCore(1000, EWarpcoreFillType.deuterium):
+}
+
+While(Colony.StockRoom.Amount(EGoodsType.Deuterium) > MinDeutOnColony AND Station.StockRoom.FreeStorage() > 0 AND (Station.Definition.WarpCore - Station.WarpCore) > 1){
+  Var Amount As Integer = Colony.StockRoom.Amount(EGoodsType.Deuterium) - MinDeutOnColony;
+  Station.Action.TransferFromColony(ColonyID, Amount, EBeamResource.Deuterium);
+  Station.Action.RefillWarpCore(Amount, EWarpcoreFillType.deuterium):
+}
